7eb3ed67eb8294672c1cc364bccfd13e425408ba
nishantkumar <nishant.soft04@gmail.com>
2021-01-17 23:17:27 +0530

    Allowing cros origin calls

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(cookieSession({
 	keys: ['niszx', 'xzsin']
 }));
 
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const https = require('https');
 
 const proxy = [
